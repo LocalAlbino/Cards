68a69761039f8a3b1b808e8d623417715b5e8df6
LocalAlbino <33205988+RudyHartwig@users.noreply.github.com>
2024-05-30 14:18:15 -0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,61 +1,59 @@
 import random
+import unicodedata
 
-# Define constants for card suits and unicode.
-SPADES = "A"
-HEARTS = "B"
-DIAMONDS = "C"
-CLUBS = "D"
-
-# Define constants for card values in ASCII. Will be iterated in a loop
-ACE = 47
-JACK = 65
-
-# Used to initialize cards to face down.
-BLANK = "0"
-UNICODE = "\U0001F0A0"
+# Define constants for card suits and value.
+SUIT = [
+    "Spades",
+    "Hearts",
+    "Diamonds",
+    "Clubs",
+]
+VALUE = [
+    "Ace",
+    "Teo",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Jack",
+    "Queen",
+    "King"
+]
 
 class Card:
-    # Blank card by default.
-    suit = SPADES
-    value = BLANK
-    face = UNICODE
-    isDiscarded = False
-    
+    # Blank card by default
+    def __init__(self):
+        self.suit = SUIT[0]
+        self.value = VALUE[0]
+        self.face = unicodedata.lookup("Playing Card " + self.value)
+        self.isDiscarded = False
+        
     def setCard(self, suit, value):
         self.suit = suit
         self.value = value
-        # FIXME: Convert list back to unicode to correctly display cards.
-        face = list(self.face.encode())
-        face[8] = suit
-        face[9] = value
-
+        if value != BACK:
+            self.face = unicodedata.lookup("Playing Card " + value + " of " + suit)
+        else:
+            self.face = unicodedata.lookup("Playing Card " + value)
+    
     def discard(self):
         self.isDiscarded = True
     
     def print(self):
         print(self.face)
         
-def initializeDeck(start, end, suit, value, deck):
-    for i in range(start, end):
-        deck[i].setCard(suit, str(chr(value)))
-        if value != 66:
-            value += 1
-        else:
-            value += 2
+def initializeDeck(deck):
+    n = 0
+    for i in SUIT:
+        for j in VALUE:
+            deck[n
+            
 
 # Declare deck as a list of 52 cards.
 deck = [Card()] * 52
 
-# Initialize each suit.
-initializeDeck(0, 10, SPADES, ACE, deck)
-initializeDeck(10, 13, SPADES, JACK, deck)
-initializeDeck(13, 23, HEARTS, ACE, deck)
-initializeDeck(23, 26, HEARTS, JACK, deck)
-initializeDeck(26, 36, DIAMONDS, ACE, deck)
-initializeDeck(36, 39, DIAMONDS, JACK, deck)
-initializeDeck(39, 49, CLUBS, ACE, deck)
-initializeDeck(49, 52, CLUBS, JACK, deck)
-
-# TEST: Print card values to ensure they appear correctly.
-for i in range(52):
-    deck[i].print()
+initializeDeck(deck)
